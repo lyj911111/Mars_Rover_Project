2052464cf2420038c94d4bfc0479742e0cbb0520
jpro <jpro6679@naver.com>
2018-09-27 13:29:59 +0900

    controller

--- Src/main.c
+++ Src/main.c
@@ -63,13 +63,13 @@ UART_HandleTypeDef huart3;
 
 volatile extern uint32_t WHEEL_SPEED;	// 타이머 Pulse 최대값: 180 (최고속도)
 
-volatile int rc_dutycycle_1 = 1;
-volatile int rc_dutycycle_2 = 0;
-volatile int rc_dutycycle_3 = 0;
-volatile int rc_dutycycle_4 = 0;
-volatile int rc_dutycycle_5 = 0;
-volatile int rc_dutycycle_6 = 0;
-volatile int rc_dutycycle_7 = 0;
+volatile int rc_dutycycle_1;
+volatile int rc_dutycycle_2;
+volatile int rc_dutycycle_3;
+volatile int rc_dutycycle_4;
+volatile int rc_dutycycle_5;
+volatile int rc_dutycycle_6;
+volatile int rc_dutycycle_7;
 
 char str[100] = {0,};
 volatile uint8_t flag1 = 0;
@@ -80,13 +80,13 @@ volatile uint8_t flag5 = 0;
 volatile uint8_t flag6 = 0;
 volatile uint8_t flag7 = 0;
 
-volatile uint16_t high_width_1, low_width_1;
-volatile uint16_t high_width_2, low_width_2;
-volatile uint16_t high_width_3, low_width_3;
-volatile uint16_t high_width_4, low_width_4;
-volatile uint16_t high_width_5, low_width_5;
-volatile uint16_t high_width_6, low_width_6;
-volatile uint16_t high_width_7, low_width_7;
+volatile uint16_t ch1_rising, ch1_falling;
+volatile uint16_t ch2_rising, ch2_falling;
+volatile uint16_t ch3_rising, ch3_falling;
+volatile uint16_t ch4_rising, ch4_falling;
+volatile uint16_t ch5_rising, ch5_falling;
+volatile uint16_t ch6_rising, ch6_falling;
+volatile uint16_t ch7_rising, ch7_falling;
 
 volatile uint16_t temp1, temp2, temp3, temp4, temp5, temp6, temp7;
 
@@ -124,95 +124,155 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
     Generation_pulse(htim);
 }
 
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
-{
-	HAL_GPIO_TogglePin(GPIOB, Red_LED_Pin);		//debug
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
+	GPIO_InitTypeDef my1_GPIO_InitStruct;
+	GPIO_InitTypeDef my2_GPIO_InitStruct;
+	GPIO_InitTypeDef my3_GPIO_InitStruct;
+	GPIO_InitTypeDef my4_GPIO_InitStruct;
+	GPIO_InitTypeDef my5_GPIO_InitStruct;
+	GPIO_InitTypeDef my6_GPIO_InitStruct;
+	GPIO_InitTypeDef my7_GPIO_InitStruct;
 
-	if(GPIO_Pin == GPIO_EXTI3_Pin){ 					//(EXTI PIN)PE3 : 1ch
-//		RC_Return_dutycycle(rc_dutycycle_1);
-		if(flag1){
-			flag1 = 0, high_width_1 = TIM3->CNT;
+	if(GPIO_Pin == GPIO_EXTI3_Pin){ 			//(EXTI PIN)PE3 : 1ch
+		if(my1_GPIO_InitStruct.Mode == GPIO_MODE_IT_RISING){
+			HAL_GPIO_TogglePin(GPIOB, Red_LED_Pin);		//debug
+			ch1_rising = TIM3->CNT;
+			HAL_GPIO_DeInit(GPIOE, GPIO_EXTI3_Pin);
+			my1_GPIO_InitStruct.Pin = GPIO_EXTI3_Pin;
+			my1_GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+			my1_GPIO_InitStruct.Pull = GPIO_PULLUP;
+			HAL_GPIO_Init(GPIOE, &my1_GPIO_InitStruct);
 		}
-		else{
-			flag1 = 1, low_width_1 = TIM3->CNT;
+		else {
+			ch1_falling = TIM3->CNT;
+			rc_dutycycle_1 = (40000 - ch1_rising + ch1_falling) % 40000;
+			HAL_GPIO_DeInit(GPIOE, GPIO_EXTI3_Pin);
+			my1_GPIO_InitStruct.Pin = GPIO_EXTI3_Pin;
+			my1_GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+			my1_GPIO_InitStruct.Pull = GPIO_PULLUP;
+			HAL_GPIO_Init(GPIOE, &my1_GPIO_InitStruct);
 		}
-		TIM3->CNT = 0;
-//		if(high_width_1 > low_width_1){
-//			temp1 = high_width_1;
-//			high_width_1 = low_width_1;
-//			low_width_1 = temp1;
-//		}
-		rc_dutycycle_1 = 40000 - low_width_1 + high_width_1;
-		rc_dutycycle_2 = 40000 - high_width_1 + low_width_1;
-
-
 	}
-//	else if (GPIO_Pin == GPIO_EXTI4_Pin){				//(EXTI PIN)PE4 : 2ch
-//		if(flag2){
-//			flag2 = 0, high_width_2 = TIM3->CNT;
-//		}
-//		else{
-//			flag2 = 1, low_width_2 = TIM3->CNT;
-//		}
-//		TIM3->CNT = 0;
-//		if(high_width_2 < low_width_2) 	rc_dutycycle_2 = high_width_2;
-//		else							rc_dutycycle_2 = low_width_2;
-//	}
-//	else if (GPIO_Pin == GPIO_EXTI5_Pin){				//(EXTI PIN)PE5 : 3ch
-//		if(flag3){
-//			flag3 = 0, high_width_3 = TIM3->CNT;
-//		}
-//		else{
-//			flag3 = 1, low_width_3 = TIM3->CNT;
-//		}
-//		TIM3->CNT = 0;
-//		if(high_width_3 < low_width_3) 	rc_dutycycle_3 = high_width_3;
-//		else							rc_dutycycle_3 = low_width_3;
-//	}
-//	else if (GPIO_Pin == GPIO_PIN_6){				//(EXTI PIN)PE6 : 4ch
-//		if(flag4){
-//			flag4 = 0, high_width_4 = TIM3->CNT;
-//		}
-//		else{
-//			flag4 = 1, low_width_4 = TIM3->CNT;
-//		}
-//		TIM3->CNT = 0;
-//		if(high_width_4 < low_width_4) 	rc_dutycycle_4 = high_width_4;
-//		else							rc_dutycycle_4 = low_width_4;
-//	}
-//	else if (GPIO_Pin == GPIO_PIN_7){				//(EXTI PIN)PF7 : 5ch
-//		if(flag5){
-//			flag5 = 0, high_width_5 = TIM3->CNT;
-//		}
-//		else{
-//			flag5 = 1, low_width_5 = TIM3->CNT;
-//		}
-//		TIM3->CNT = 0;
-//		if(high_width_5 < low_width_5) 	rc_dutycycle_5 = high_width_5;
-//		else							rc_dutycycle_5 = low_width_5;
-//	}
-//	else if (GPIO_Pin == GPIO_PIN_8){				//(EXTI PIN)PF8 : 6ch
-//		if(flag6){
-//			flag6 = 0, high_width_6 = TIM3->CNT;
-//		}
-//		else{
-//			flag6 = 1, low_width_6 = TIM3->CNT;
-//		}
-//		TIM3->CNT = 0;
-//		if(high_width_6 < low_width_6) 	rc_dutycycle_6 = high_width_6;
-//		else							rc_dutycycle_6 = low_width_6;
-//	}
-//	else if (GPIO_Pin == GPIO_PIN_9){				//(EXTI PIN)PF8 : 6ch
-//		if(flag7){
-//			flag7 = 0, high_width_7 = TIM3->CNT;
-//		}
-//		else{
-//			flag7 = 1, low_width_7 = TIM3->CNT;
-//		}
-//		TIM3->CNT = 0;
-//		if(high_width_7 < low_width_7) 	rc_dutycycle_7 = high_width_7;
-//		else							rc_dutycycle_7 = low_width_7;
-//	}
+
+	else if (GPIO_Pin == GPIO_EXTI4_Pin){				//(EXTI PIN)PE4 : 2ch
+		if(my2_GPIO_InitStruct.Mode == GPIO_MODE_IT_RISING){
+			ch2_rising = TIM3->CNT;
+			HAL_GPIO_DeInit(GPIOE, GPIO_EXTI4_Pin);
+			my2_GPIO_InitStruct.Pin = GPIO_EXTI4_Pin;
+			my2_GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+			my2_GPIO_InitStruct.Pull = GPIO_PULLUP;
+			HAL_GPIO_Init(GPIOE, &my2_GPIO_InitStruct);
+		}
+		else {
+			ch2_falling = TIM3->CNT;
+			rc_dutycycle_2 = (40000 - ch2_rising + ch2_falling) % 40000;
+			HAL_GPIO_DeInit(GPIOE, GPIO_EXTI4_Pin);
+			my2_GPIO_InitStruct.Pin = GPIO_EXTI4_Pin;
+			my2_GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+			my2_GPIO_InitStruct.Pull = GPIO_PULLUP;
+			HAL_GPIO_Init(GPIOE, &my2_GPIO_InitStruct);
+		}
+	}
+
+	else if (GPIO_Pin == GPIO_EXTI5_Pin){				//(EXTI PIN)PE5 : 3ch
+		if(my3_GPIO_InitStruct.Mode == GPIO_MODE_IT_RISING){
+			ch3_rising = TIM3->CNT;
+			HAL_GPIO_DeInit(GPIOE, GPIO_EXTI5_Pin);
+			my3_GPIO_InitStruct.Pin = GPIO_EXTI5_Pin;
+			my3_GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+			my3_GPIO_InitStruct.Pull = GPIO_PULLUP;
+			HAL_GPIO_Init(GPIOE, &my3_GPIO_InitStruct);
+		}
+		else {
+			ch3_falling = TIM3->CNT;
+			rc_dutycycle_3 = (40000 - ch3_rising + ch3_falling) % 40000;
+			HAL_GPIO_DeInit(GPIOE, GPIO_EXTI5_Pin);
+			my3_GPIO_InitStruct.Pin = GPIO_EXTI5_Pin;
+			my3_GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+			my3_GPIO_InitStruct.Pull = GPIO_PULLUP;
+			HAL_GPIO_Init(GPIOE, &my3_GPIO_InitStruct);
+		}
+	}
+
+	else if (GPIO_Pin == GPIO_EXTI6_Pin){				//(EXTI PIN)PE6 : 4ch
+		if(my4_GPIO_InitStruct.Mode == GPIO_MODE_IT_RISING){
+			ch4_rising = TIM3->CNT;
+			HAL_GPIO_DeInit(GPIOE, GPIO_EXTI6_Pin);
+			my4_GPIO_InitStruct.Pin = GPIO_EXTI6_Pin;
+			my4_GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+			my4_GPIO_InitStruct.Pull = GPIO_PULLUP;
+			HAL_GPIO_Init(GPIOE, &my4_GPIO_InitStruct);
+		}
+		else {
+			ch4_falling = TIM3->CNT;
+			rc_dutycycle_4 = (40000 - ch4_rising + ch4_falling) % 40000;
+			HAL_GPIO_DeInit(GPIOE, GPIO_EXTI6_Pin);
+			my4_GPIO_InitStruct.Pin = GPIO_EXTI6_Pin;
+			my4_GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+			my4_GPIO_InitStruct.Pull = GPIO_PULLUP;
+			HAL_GPIO_Init(GPIOE, &my4_GPIO_InitStruct);
+		}
+	}
+
+	else if (GPIO_Pin == GPIO_EXTI7_Pin){				//(EXTI PIN)PF7 : 5ch
+		if(my5_GPIO_InitStruct.Mode == GPIO_MODE_IT_RISING){
+			ch5_rising = TIM3->CNT;
+			HAL_GPIO_DeInit(GPIOF, GPIO_EXTI7_Pin);
+			my5_GPIO_InitStruct.Pin = GPIO_EXTI7_Pin;
+			my5_GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+			my5_GPIO_InitStruct.Pull = GPIO_PULLUP;
+			HAL_GPIO_Init(GPIOF, &my5_GPIO_InitStruct);
+		}
+		else {
+			ch5_falling = TIM3->CNT;
+			rc_dutycycle_5 = (40000 - ch5_rising + ch5_falling) % 40000;
+			HAL_GPIO_DeInit(GPIOF, GPIO_EXTI7_Pin);
+			my5_GPIO_InitStruct.Pin = GPIO_EXTI7_Pin;
+			my5_GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+			my5_GPIO_InitStruct.Pull = GPIO_PULLUP;
+			HAL_GPIO_Init(GPIOF, &my5_GPIO_InitStruct);
+		}
+	}
+
+	else if (GPIO_Pin == GPIO_EXTI8_Pin){				//(EXTI PIN)PF8 : 6ch
+		if(my6_GPIO_InitStruct.Mode == GPIO_MODE_IT_RISING){
+			ch6_rising = TIM3->CNT;
+			HAL_GPIO_DeInit(GPIOF, GPIO_EXTI8_Pin);
+			my6_GPIO_InitStruct.Pin = GPIO_EXTI8_Pin;
+			my6_GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+			my6_GPIO_InitStruct.Pull = GPIO_PULLUP;
+			HAL_GPIO_Init(GPIOF, &my6_GPIO_InitStruct);
+		}
+		else {
+			ch6_falling = TIM3->CNT;
+			rc_dutycycle_6 = (40000 - ch6_rising + ch6_falling) % 40000;
+			HAL_GPIO_DeInit(GPIOF, GPIO_EXTI8_Pin);
+			my6_GPIO_InitStruct.Pin = GPIO_EXTI8_Pin;
+			my6_GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+			my6_GPIO_InitStruct.Pull = GPIO_PULLUP;
+			HAL_GPIO_Init(GPIOF, &my6_GPIO_InitStruct);
+		}
+	}
+
+	else if (GPIO_Pin == GPIO_EXTI9_Pin){				//(EXTI PIN)PF8 : 6ch
+		if(my7_GPIO_InitStruct.Mode == GPIO_MODE_IT_RISING){
+			ch7_rising = TIM3->CNT;
+			HAL_GPIO_DeInit(GPIOF, GPIO_EXTI9_Pin);
+			my7_GPIO_InitStruct.Pin = GPIO_EXTI9_Pin;
+			my7_GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+			my7_GPIO_InitStruct.Pull = GPIO_PULLUP;
+			HAL_GPIO_Init(GPIOF, &my7_GPIO_InitStruct);
+		}
+		else {
+			ch7_falling = TIM3->CNT;
+			rc_dutycycle_7 = (40000 - ch7_rising + ch7_falling) % 40000;
+			HAL_GPIO_DeInit(GPIOF, GPIO_EXTI9_Pin);
+			my7_GPIO_InitStruct.Pin = GPIO_EXTI9_Pin;
+			my7_GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+			my7_GPIO_InitStruct.Pull = GPIO_PULLUP;
+			HAL_GPIO_Init(GPIOF, &my7_GPIO_InitStruct);
+		}
+	}
 
 
 }
@@ -536,9 +596,9 @@ static void MX_TIM3_Init(void)
   TIM_MasterConfigTypeDef sMasterConfig;
 
   htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 180;
+  htim3.Init.Prescaler = 45;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 20000;
+  htim3.Init.Period = 40000;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
   {
@@ -643,7 +703,7 @@ static void MX_GPIO_Init(void)
 
   /*Configure GPIO pins : GPIO_EXTI3_Pin GPIO_EXTI4_Pin GPIO_EXTI5_Pin GPIO_EXTI6_Pin */
   GPIO_InitStruct.Pin = GPIO_EXTI3_Pin|GPIO_EXTI4_Pin|GPIO_EXTI5_Pin|GPIO_EXTI6_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
@@ -655,7 +715,7 @@ static void MX_GPIO_Init(void)
 
   /*Configure GPIO pins : GPIO_EXTI7_Pin GPIO_EXTI8_Pin GPIO_EXTI9_Pin */
   GPIO_InitStruct.Pin = GPIO_EXTI7_Pin|GPIO_EXTI8_Pin|GPIO_EXTI9_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
 
